c4f2847059355f1a5ca44160ddacd3cee52c0272
yutarotani <kodatinitoryu@yahoo.co.jp>
2026-03-06 01:58:49 +0900

    画像アップロード機能追加

--- fixedcut_app/__init__.py
+++ fixedcut_app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+UPLOAD_FOLDER = './upload'
+ALLOWED_EXTENSIONS = set(['eps'])
+
 app = Flask(__name__, static_folder='./templates/static')
 app.config.from_object('fixedcut_app.config') 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
 from.models import fixedcut, senkyo_person, senkyo_sendgroup
--- fixedcut_app/views.py
+++ fixedcut_app/views.py
@@ -1,5 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from datetime import datetime
 from fixedcut_app import app
+from fixedcut_app.models.fixedcut import FixedCut
+import os, pathlib
+
 
 @app.route('/')
 def index():
@@ -41,9 +45,63 @@ def general():
 
 @app.route('/general_add', methods=['GET', 'POST'])
 def general_add():
-    return render_template('general_add')
+    if request.method == 'GET':
+        return render_template('general_add.html')
+    
+    if request.method == 'POST':
+        form_id = request.form.get("id")
+        form_midashi = request.form.get("midashi")
+        form_Str = request.form.get("Str")
+        form_colorUrl = request.files.get("colorUrl")
+        form_monoUrl = request.files.get("monoUrl")
+        form_updateFlg = request.form.get("updateFlg")
 
+        try:
+            os.makedirs(f'fixedcut_app/upload/{form_id}/color')
+        except(FileExistsError):
+            pass
+            
+        try: 
+            os.makedirs(f'fixedcut_app/upload/{form_id}/mono')
+        except(FileExistsError):
+            pass
+        
+        color_name = form_colorUrl.filename
+        mono_name = form_monoUrl.filename
+
+        color_path = pathlib.Path(f'fixedcut_app/upload/{form_id}/color' ,color_name)
+        mono_path = pathlib.Path(f'fixedcut_app/upload/{form_id}/mono' ,mono_name)
+        try:
+            form_colorUrl.save(color_path)
+        except(PermissionError):
+            pass
+        
+        try:
+            form_monoUrl.save(mono_path)
+        except(PermissionError):
+            pass
+
+        fixedcut = FixedCut(
+            id = form_id, #固定カットID
+            midashi = form_midashi, #仮見出し
+            Str = form_Str, #一体化時文字列
+            colorUrl = color_path, #カラー画像のURL
+            monoUrl = mono_path, #モノクロ画像のURL
+            updateFlg = form_updateFlg, #モノクロ画像のURL
+            created_at = datetime.now,  # 作成日時
+            updated_at = datetime.now #更新日時
+        )
+        print(fixedcut)
+        #db.session.add(fixedcut)
+        #db.session.commit()
+        return render_template('general_add.html')
 
 @app.route('/general_detail')
 def general_detail():
     return render_template('general_detail.html')
+
+
+def allwed_file(filename):
+    # .があるかどうかのチェックと、拡張子の確認
+    # OKなら１、だめなら0
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
